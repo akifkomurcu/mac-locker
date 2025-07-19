8d559eccb1d9b99686dc5eb1ee3f5d5341013532
Akif Kömürcü <akif.komurcu@ext.etiya.com>
2025-07-19 23:01:54 +0300

    Initial Commit

---
+++ mac-locker/mac_lockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_lockerApp.swift
+//  mac-locker
+//
+//  Created by KOMURCU on 19.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct mac_lockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mac-lockerTests/mac_lockerTests.swift
@@ -0,0 +1,17 @@
+//
+//  mac_lockerTests.swift
+//  mac-lockerTests
+//
+//  Created by KOMURCU on 19.07.2025.
+//
+
+import Testing
+@testable import mac_locker
+
+struct mac_lockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
